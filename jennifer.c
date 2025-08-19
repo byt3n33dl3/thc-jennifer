ce8729a3a1278df6662cb25767ef314ab6c5b88d
Sulaiman <sulabaziz09@gmail.com>
2025-08-19 22:19:09 +0800

    0x00 Technical Fix Analysis
    0x00

--- jennifer.c
+++ jennifer.c
@@ -393,7 +393,7 @@ void print_banner() {
     printf("⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀\n");
     printf("⠀⠀⠐⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂\n");
     printf("\nJennifer: KeePass Password Cracker v%s\n", VERSION);
-    printf("Current User: @byt3n33dl3, 2025 UTC\n\n");
+    printf("Current User: @byt3n33dl3, UTC\n\n");
 }
 
 void print_usage(const char *program_name) {
